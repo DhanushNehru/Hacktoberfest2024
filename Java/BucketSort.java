6f8e67954a0165b1a59a4baa01e5b0c2276f8c34
dvir019 <30556126+dvir019@users.noreply.github.com>
2024-10-16 16:59:39 +0300

    Implemented bucket sort of integers in Java

--- Java/BucketSort.java
+++ Java/BucketSort.java
@@ -1,40 +1,29 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class BucketSort {
-  public void bucketSort(float[] arr, int n) {
-    if (n <= 0)
-      return;
-    @SuppressWarnings("unchecked")
-    ArrayList<Float>[] bucket = new ArrayList[n];
+    public static void bucketSort(int[] arr, int low, int high) {
+        int[] hist = new int[high - low + 1];
 
-    for (int i = 0; i < n; i++)
-      bucket[i] = new ArrayList<Float>();
+        for (int num : arr) {
+            hist[num - low]++;
+        }
 
-    for (int i = 0; i < n; i++) {
-      int bucketIndex = (int) arr[i] * n;
-      bucket[bucketIndex].add(arr[i]);
+        int index = 0;
+
+        for (int i = 0; i < hist.length; i++) {
+            for (int j = 0; j < hist[i]; j++) {
+                arr[index++] = i + low;
+            }
+        }
     }
 
-    for (int i = 0; i < n; i++) {
-      Collections.sort((bucket[i]));
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 5, 2, 2, 3, 0, 0, -1, 2};
+
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
+
+        bucketSort(arr, -1, 5);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
     }
-
-    int index = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0, size = bucket[i].size(); j < size; j++) {
-        arr[index++] = bucket[i].get(j);
-      }
-    }
-  }
-
-  public static void main(String[] args) {
-    BucketSort b = new BucketSort();
-    float[] arr = { (float) 0.42, (float) 0.32, (float) 0.33, (float) 0.52, (float) 0.37, (float) 0.47,
-        (float) 0.51 };
-    b.bucketSort(arr, 7);
-
-    for (float i : arr)
-      System.out.print(i + "  ");
-  }
 }
