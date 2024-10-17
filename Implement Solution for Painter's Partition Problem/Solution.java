8d06949ad519778d51b61c57127aa42d4f68e393
Anuska Chatterjee <144933964+anuska2772@users.noreply.github.com>
2024-10-17 12:22:47 +0530

    Create Solution.java
    This is the most optimized solution using Binary Search.

---
+++ Implement Solution for Painter's Partition Problem/Solution.java
@@ -0,0 +1,48 @@
+
+
+
+import java.util.*;
+
+public class tUf {
+    public static int countPainters(ArrayList<Integer> boards, int time) {
+        int n = boards.size(); // size of array.
+        int painters = 1;
+        long boardsPainter = 0;
+        for (int i = 0; i < n; i++) {
+            if (boardsPainter + boards.get(i) <= time) {
+                // allocate board to current painter
+                boardsPainter += boards.get(i);
+            } else {
+                // allocate board to next painter
+                painters++;
+                boardsPainter = boards.get(i);
+            }
+        }
+        return painters;
+    }
+
+    public static int findLargestMinDistance(ArrayList<Integer> boards, int k) {
+        int low = Collections.max(boards);
+        int high = boards.stream().mapToInt(Integer::intValue).sum();
+
+        // Apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int painters = countPainters(boards, mid);
+            if (painters > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> boards = new ArrayList<>(Arrays.asList(10, 20, 30, 40));
+        int k = 2;
+        int ans = findLargestMinDistance(boards, k);
+        System.out.println("The answer is: " + ans);
+    }
+}
+
