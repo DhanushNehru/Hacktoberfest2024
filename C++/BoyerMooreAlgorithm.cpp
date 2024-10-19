924411f07c7c41ffb65385958b3e8e8742209459
gauravbhaskar080 <gauravbhaskar111@gmail.com>
2024-10-19 15:57:49 +0530

    Boyer Moore Algorithm for pattern searching

---
+++ C++/BoyerMooreAlgorithm.cpp
@@ -0,0 +1,59 @@
+/*
+Boyer Moore Algorithm for Pattern Searching :
+Pattern searching is an important problem in computer science. When we do search for a string in a notepad/word file, browser, 
+or database, pattern searching algorithms are used to show the search results.
+
+A typical problem statement would be- 
+"Given a text txt[0..n-1] and a pattern pat[0..m-1] where n is the length of the text and m is the length of the pattern, 
+write a function search(char pat[], char txt[]) that prints all occurrences of pat[] in txt[]. You may assume that n > m."
+
+Examples:
+Input:  txt[] = "THIS IS A TEST TEXT"
+        pat[] = "TEST"
+Output: Pattern found at index 10
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NO_OF_CHARS 256
+
+void badCharHeuristic(string str, int size,int badchar[NO_OF_CHARS]){
+    int i;
+    for (i = 0; i < NO_OF_CHARS; i++)
+        badchar[i] = -1;
+ 
+    for (i = 0; i < size; i++)
+        badchar[(int)str[i]] = i;
+}
+
+void search(string txt, string pat){
+    int m = pat.size();
+    int n = txt.size();
+ 
+    int badchar[NO_OF_CHARS];
+    badCharHeuristic(pat, m, badchar);
+ 
+    int s = 0; 
+    while (s <= (n - m)) {
+        int j = m - 1;
+
+        while (j >= 0 && pat[j] == txt[s + j])
+            j--;
+
+        if (j < 0) {
+            cout << "pattern occurs at shift = " << s<< endl;
+
+            s += (s + m < n) ? m - badchar[txt[s + m]] : 1;
+        }
+ 
+        else s += max(1, j - badchar[txt[s + j]]);
+    }
+}
+ 
+int main(){
+    string txt = "ABAAABCD";
+    string pat = "ABC";
+    search(txt, pat);
+    return 0;
+}
---
+++ content/participant/GauravBhaskar.md
@@ -0,0 +1,5 @@
+---
+name: Gaurav Bhaskar
+institution/company: Indian Institute of Information Technology, Sri City, Chittoor, Andhra Pradesh
+github: [gauravbhaskar080](https://github.com/gauravbhaskar080)
+---
