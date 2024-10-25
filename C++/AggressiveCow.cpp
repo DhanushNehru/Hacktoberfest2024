114163c0906beb37514a45142a5be8e71227ee9a
AbhishekChamp879 <abhishekkumargiri770@gmail.com>
2024-10-25 23:37:34 +0530

    BinarySearch_Algorithm

---
+++ C++/AggressiveCow.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canWePlace(vector<int> &stalls, int dist, int cows) {
+    int n = stalls.size(); //size of array
+    int cntCows = 1; //no. of cows placed
+    int last = stalls[0]; //position of last placed cow.
+    for (int i = 1; i < n; i++) {
+        if (stalls[i] - last >= dist) {
+            cntCows++; //place next cow.
+            last = stalls[i]; //update the last location.
+        }
+        if (cntCows >= cows) return true;
+    }
+    return false;
+}
+int aggressiveCows(vector<int> &stalls, int k) {
+    int n = stalls.size(); //size of array
+    //sort the stalls[]:
+    sort(stalls.begin(), stalls.end());
+
+    int low = 1, high = stalls[n - 1] - stalls[0];
+    //apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (canWePlace(stalls, mid, k) == true) {
+            low = mid + 1;
+        }
+        else high = mid - 1;
+    }
+    return high;
+}
+
+int main()
+{
+    vector<int> stalls = {0, 3, 4, 7, 10, 9};
+    int k = 4;
+    int ans = aggressiveCows(stalls, k);
+    cout << "The maximum possible minimum distance is: " << ans << "\n";
+    return 0;
+}
+
