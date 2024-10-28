7906d2f40822b1dfabbb6cc392a840afbd535aa4
Isha Patil <124502923+PatilIsha@users.noreply.github.com>
2024-10-28 22:08:10 +0530

    Kadane's Algorithm to find the maximum sum of a contiguous subarray #338te Kadane'sAlgo.java

--- Kadane'sAlgo.java
+++ Kadane'sAlgo.java
@@ -12,3 +12,22 @@ class Solution {
         return maxs;
     }
 }
+/////////////////////////////
+public class KadaneAlgorithm {
+    public static int maxSubArraySum(int[] arr) {
+        int maxSoFar = arr[0];
+        int maxEndingHere = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        return maxSoFar;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Maximum sum of contiguous subarray: " + maxSubArraySum(arr));
+    }
+}
